200860901fc872a0dc7a9d85dac68a3d2f5f9777
Rita-Gomez <ritagomezh@gmail.com>
2022-11-08 12:10:20 -0500

    finished js and css-catching err

--- src/data.js
+++ src/data.js
@@ -1,20 +1,23 @@
-let weather = (city)=>{
-  fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=bbf1293e8b911e028baf57b0d801f076")
-  .then(response => response.json())
-  .then(data => {
+let weather = (city) => {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=bbf1293e8b911e028baf57b0d801f076"
+  )
+    .then((response) => response.json())
+    .then((data) => {
       displayData(data);
-})}
+    })
+    .catch((err) => notFound(err));
+};
 
-document.getElementById('submit').addEventListener('click' , ()=>{
-  return weather( document.getElementById('city').value);
-})
- 
-document.getElementById("city").addEventListener("keyup",(event)=>{
-  if (event.key == "Enter"){
-    weather(document.getElementById('city').value);
-  }}) 
+weather("lima");
+document.getElementById("submit").addEventListener("click", () => {
+  return weather(document.getElementById("city").value);
+});
 
-
-
-
-    
+document.getElementById("city").addEventListener("keyup", (event) => {
+  if (event.key == "Enter") {
+    weather(document.getElementById("city").value);
+  }
+});
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
- const displayData = (data)=>{
-  console.log(data)
-  return document.querySelector('.display').innerHTML = 
-  `<h1 style="font-size:3rem">${data.name}</h1>
+const displayData = (data) => {
+  console.log(data);
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1600x900/?" + data.name + "')";
+  document.querySelector(
+    ".display"
+  ).innerHTML = `<h1 style="font-size:3rem">${data.name}</h1>
   <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" width="70px" height="70px" text-align="center">
   <h2 style="font-size: 3rem;">${data.main.temp} ºC </h2>
   <p class="description">${data.weather[0].description}</p>
-  <p class="description">Humedad : ${data.main.humidity} % </p>`
-  
-  /*weather:[{id: 800, main: 'Clear', description: 'clear sky', icon: '01d'}] 
-  main :{
-    humidity: 27
-    temp: 30.92
-  }
- name:"Qatar" */
-
-} 
+  <p class="description">Humedad : ${data.main.humidity} % </p>`;
+};
+const notFound = (err) => {
+  return (document.querySelector(
+    ".display"
+  ).innerHTML = `<h1>Ups! Esta ciudad no existe</h1>`);
+};
